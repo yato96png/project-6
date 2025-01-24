4aad938076baa2373fdd93c12165bda8226ae246
vizal123 <yato9png@gmail.com>
2025-01-24 13:06:41 +0500

    Create html.js

---
+++ html.js
@@ -0,0 +1,12 @@
+function buildPyramid(height) {
+  for (let i = 1; i <= height; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+      row += j;
+    }
+    console.log(row);
+  }
+}
+
+let height = 5;
+buildPyramid(height);
